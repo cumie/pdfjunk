d784f06b59f95f7a1070c5841cd8b430e76b4bf0
cumie <havid.aide@gmail.com>
2017-12-21 00:04:41 +0800

    finish halaman preview rekapitulasi

--- pages/laporan/footer.php
+++ pages/laporan/footer.php
@@ -1,17 +1,21 @@
-<table class="tg" align="center" width="980" border="0">
+<table class="headtab" align="center" width="980" border="0">
   <tr>
     <td width="400">*) Coret yang Bungas</td>
     <td width="250">&nbsp;</td>
     <td width="250" align="left"></td>
   </tr>    
   <tr> 
-    <td rowspan="7">&nbsp;</td>
+    <td rowspan="8">&nbsp;</td>
     <td>&nbsp;</td>
     <td align="left">Ketua Pejabat</td>
   </tr> 
    <tr>
     <td>&nbsp;</td>
     <td align="left">Universitas Nang Isinya Keluarga Ako</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="g3" align="center">&nbsp;</td>
   </tr>
    <tr>
     <td>&nbsp;</td>
