4f6baf71d2ded97e067dcb039a1d9debc6f0d57e
gnosprinting <anjar.hexacore@gmail.com>
2017-12-21 16:42:27 +0800

    head4 footer4 for lampiran4

---
+++ pages/laporan/footer4.php
@@ -0,0 +1,60 @@
+<table class="tfa" >
+  <tr>
+    <td class="thaka" width="470">Demikian Pernyataan ini dibuat untuk dapat dipergunakan sebagaimana mestinya</td>
+    <td width="60">&nbsp;</td>
+    <td width="150" align="left"></td>
+  </tr>
+  <tr>
+    <td rowspan="8">&nbsp;</td>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left">Banjarmasin, 31 Desember 2017</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left">Yang Membuat Pernyataan</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left">Ketua Universitas </td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="" align="center">&nbsp;</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="" align="center">&nbsp;</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="" align="center">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left"><u>Fathul Hafidh</u></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left">NIP. 1985110506031</td>
+  </tr>
+  <tr>
+    <td class="kcl bold"><u>Catatan:</u></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td class="thaki">1. Dibuat per semester.</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td class="thaki">2. Ditanda tangani oleh Ketua Jurusan pada Univ/Insititut/Sekolah Tinggi/Akademi/Politeknik.</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td class="thaki">3. Dilampirkan surat penugasan tersebut di atas.</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
---
+++ pages/laporan/head4.php
@@ -0,0 +1,134 @@
+<?php
+$now = date("Y-m-d");
+		// $SQL = "SELECT tunitkerja.* FROM tunitkerja WHERE id_unitkerja=".$_SESSION['4XE']." ";
+		// $myQry = mysqli_query($koneksidb, $SQL)  or die ("Query Lurah : ".mysqli_error($koneksidb));
+		// $datane = mysqli_fetch_array($myQry);
+		// $today = date("Y-m-d");
+?>
+<link href="style-pdf.css" rel="stylesheet" type="text/css" />
+
+<table class="tha" border="0">
+	<tbody>
+		<tr>
+			<td width="500" align="right" rowspan="">LAMPIRAN IV</td>
+			<td width="120" colspan="3">KEPUTUSAN BERSAMA MENTERI</td>
+		</tr>
+		<tr>
+			<td width="" align="right" rowspan="5"></td>
+			<td colspan="3">PENDIDIKAN DAN KEBUDAYAAN DAN</td>
+		</tr>
+		<tr>
+			<td colspan="3">KEPALA BADAN KEPEGAWAIAN NEGARA</td>
+		</tr>
+		<tr>
+			<td width="60">NOMOR</td>
+			<td width="5">:</td>
+			<td>61409/MPK/KP/99</td>
+		</tr>
+		<tr>
+			<td>NOMOR</td>
+			<td>:</td>
+			<td>181 TAHUN 1999</td>
+		</tr>
+		<tr>
+			<td>TANGGAL</td>
+			<td>:</td>
+			<td>13 OKTOBER 1999</td>
+		</tr>
+	</tbody>
+</table>
+
+<table class="headtab" align="center" width="760" border="0">
+  <tr>
+    <td width="700" colspan="2" align="center"><h4>SURAT PERNYATAAN</h4></td>
+  </tr>
+  <tr>
+    <td width="700" colspan="2" align="center"><h4>MELAKSANAKAN KEGIATAN PENGABDIAN PADA MASYARAKAT</h4></td>
+  </tr>
+
+  <tr>
+  <td width="220" align="right"> </td>
+  <td>  </td>
+  </tr>
+
+
+  </table>
+<table class="tha" border="0">
+	<tbody>
+		<tr>
+			<td width="10" rowspan="12"></td>
+			<td width="20" colspan="2">Yang Bertanda tangan dibawah ini</td>
+			<td width="5">:</td>
+			<td width="810"></td>
+		</tr>
+		<tr>
+			<td width="10" rowspan="5"></td>
+			<td width="50">Nama</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>NIP / NIK </td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Pangkat / Golongan Ruang</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Jabatan Fungsional</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Unit Kerja</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td colspan="2">Menyatakan bahwa</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td rowspan="5"></td>
+			<td>Nama</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>NIP / NIK </td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Pangkat / Golongan Ruang</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Jabatan Fungsional</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Unit Kerja</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+<br/>
+<table class="tha">
+	<tr>
+		<td width="20">&nbsp;</td>
+		<td width="50" colspan="2">
+			Telah Melakukan kegiatan Pengabdian pada Masyarakat sebagai berikut
+		</td>
+		<td width="5">:</td>
+		<td width="640"></td>
+	</tr>
+</table>
+<br/>
