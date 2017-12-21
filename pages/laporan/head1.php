66a26cbf5993b4f56782079924be7e7b9d0ad1c4
cumie <havid.aide@gmail.com>
2017-12-21 10:43:49 +0800

    header lampiran 1

---
+++ pages/laporan/head1.php
@@ -0,0 +1,22 @@
+<?php
+$now = date("Y-m-d");
+		// $SQL = "SELECT tunitkerja.* FROM tunitkerja WHERE id_unitkerja=".$_SESSION['4XE']." ";
+		// $myQry = mysqli_query($koneksidb, $SQL)  or die ("Query Lurah : ".mysqli_error($koneksidb)); 
+		// $datane = mysqli_fetch_array($myQry); 
+		// $today = date("Y-m-d");
+?>
+<link href="style-pdf.css" rel="stylesheet" type="text/css" /> 
+<table class="headtab" align="center" width="760" border="0">
+  <tr>
+    <td width="700" colspan="2" align="center"><h4>DAFTAR USUL PENETAPAN ANGKA KREDIT</h4></td> 
+  </tr> 
+  <tr>
+    <td width="700" colspan="2" align="center"><h4>JABATAN FUNGSIONAL DOSEN</h4></td> 
+  </tr> 
+   
+  <tr>
+  <td width="220" align="right">TANGGAL PENILAIAN:</td>
+  <td> TGL. 1 September 2017 S/D TGL. 30 APRIL 2017</td>
+  </tr>
+  </table>
+<br/> 
