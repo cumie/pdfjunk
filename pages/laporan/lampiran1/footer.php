172ec0c7f5a14aa633f597bb536205f81d40d1f1
gnosprinting <anjar.hexacore@gmail.com>
2017-12-21 13:59:53 +0800

    head foot lampiran 1

---
+++ pages/laporan/lampiran1/footer.php
@@ -0,0 +1,64 @@
+<table class="headtab" align="center" width="845" border="0">
+  <tr>
+    <td width="540" class="g5">Demikian pernyataan ini dibuat untuk dapat dipergunakan sebagaimana mestinya.</td>
+    <td width="5">&nbsp;</td>
+    <td width="300" align="left"></td>
+  </tr>
+  <tr>
+    <td rowspan="9">&nbsp;</td>
+    <td>&nbsp;</td>
+    <td align="left">Banjarbaru, --</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="left">Yang Membuat Pernyataan</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td align="left">Puket I STIE Panca Setia Banjarmasin</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="g3" align="center">&nbsp;</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="g3" align="center">&nbsp;</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="g3" align="center">&nbsp;</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="g3" align="center">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="left"><b><u>Sutrisno, SE., MM</u></b></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="left">NIP. 19670512 200501 1 001</td>
+  </tr>
+  <tr>
+    <td><u>Catatan:</u></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td class="g5">1. Dibuat per semester</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td class="g5">2. Ditanda tangani oleh Ketua Jurusan pada Univ/Institut/Sekolah Tinggi/Akademi/Politeknik.</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td class="g5">3. Dilampirkan surat penugasan tersebut diatas.</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
---
+++ pages/laporan/lampiran1/head.php
@@ -0,0 +1,49 @@
+<?php
+$now = date("Y-m-d");
+		// $SQL = "SELECT tunitkerja.* FROM tunitkerja WHERE id_unitkerja=".$_SESSION['4XE']." ";
+		// $myQry = mysqli_query($koneksidb, $SQL)  or die ("Query Lurah : ".mysqli_error($koneksidb));
+		// $datane = mysqli_fetch_array($myQry);
+		// $today = date("Y-m-d");
+?>
+<link href="style-pdf.css" rel="stylesheet" type="text/css" />
+<br>
+<table class="ta" align="left" width="" border="0">
+  <tr>
+    <td class="" align="right" width="500">
+        LAMPIRAN II <br>
+        &nbsp;<br>
+        &nbsp;
+    </td>
+    <td class="" align="left" width="440" colspan="3">
+        KEPUTUSAN BERSAMA MENTERI <br>
+        PENDIDIKAN DAN KEBUDAYAAN DAN <br>
+        KEPALA BADAN KEPEGAWAIAN NEGARA
+    </td>
+  </tr>
+  <tr>
+    <td rowspan="3"></td>
+    <td class="" align="left" width="50" align="left">
+      NOMOR
+    </td>
+    <td width="5">:</td>
+    <td>61409/MPK/KP/99</td>
+  </tr>
+  <tr>
+    <td class="" align="left" width="" align="left">
+      NOMOR
+    </td>
+    <td width="5">:</td>
+    <td>181 TAHUN 1999</td>
+  </tr>
+  <tr>
+    <td class="" align="left" width="" align="left">
+      TANGGAL
+    </td>
+    <td width="5">:</td>
+    <td>13 OKTOBER 1999</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<br/><br>
