cced042ad5bcc96b7c23b4e10093a1d2273dd57a
gnosprinting <anjar.hexacore@gmail.com>
2017-12-21 19:54:39 +0800

    lampiran3
    kd kawa sama persis, di rowspan tehambur

---
+++ pages/laporan/lampiran3.php
@@ -0,0 +1,149 @@
+<?php
+session_start();
+// include "../../lib/connection.php";
+// include "../../lib/library.php";
+//include_once "../masters/dosen_config.php";
+
+
+// include_once "lib/seslogin.php";
+// include_once "pages/masters/dosen_config.php";
+// if($_GET) {
+
+// $Kode	 = $_SESSION['ID_CRAB'];
+// $sqlShow = "SELECT * FROM ".$tableName." WHERE ".$field[4]."='$Kode'";
+// $qryShow = mysqli_query($koneksidb, $sqlShow)  or die ("Query ambil data salah : ".mysqli_error($koneksidb));
+// $dataShow = mysqli_fetch_array($qryShow);
+
+// for($i=0;$i<=$jmlField;$i++){
+	// $data[$i] = $dataShow[$field[$i]];
+// }
+// } // Penutup GET
+?>
+
+<?php include('head3.php');
+?>
+
+<table class="tisi">
+  <tr>
+  	<th rowspan="2">NO.</th>
+		<th rowspan="2" width="400">NAMA JUDUL KARYA ILMIAH (UNSUR)</th>
+		<th width="150">SUB UNSUR</th>
+		<th colspan="2" width="300">ANGKA KREDIT MENURUT</th>
+		<th rowspan="2" width="100">KET/BUKTI FISIK</th>
+  </tr>
+	<tr>
+		<th>NILAI ANGKA KREDIT</th>
+		<th width="200">
+			DIRJEN DIKTI / REKTOR / UNIV / INSTITUT / SEK.TINGGI / DIREKTUR AKADEMI / POLITEKNIK
+		</th>
+		<th width="150">TIM PENILAI PUSAT/PTN/PTS</th>
+  </tr>
+	<tr>
+  	<th>1</th>
+		<th>2</th>
+		<th>3</th>
+		<th>4</th>
+		<th>5</th>
+		<th>6</th>
+  </tr>
+	<tr class="g5">
+		<td class="tisi-baqh bold">1</td>
+		<td>
+			Arief Noviarakhman Zagladi (2007)
+			Arti Penting Unsur Keadilan Dalam
+			Organisasi
+			Laporan Penelitian Dipublikasikan
+		</td>
+		<td class="tisi-baqh bold" align="center">10</td>
+		<td> </td>
+		<td> </td>
+		<td>
+			Jurnal Wawasan
+			Kopertis Wilayah XI
+			Volume 13 Nomor 3
+			Desember 2007
+			ISSN : 14411-4267</td>
+	</tr>
+	<tr class="g5">
+		<td class="tisi-baqh bold">
+			2
+		</td>
+		<td>
+			Arief Noviarakhman Zagladi (2007)
+			Arti Penting Unsur Keadilan Dalam
+			Organisasi
+			Laporan Penelitian Dipublikasikan
+		</td>
+		<td class="tisi-baqh bold" align="center">10</td>
+		<td> </td>
+		<td> </td>
+		<td>
+			Jurnal Wawasan
+			Kopertis Wilayah XI
+			Volume 13 Nomor 3
+			Desember 2007
+			ISSN : 14411-4267</td>
+	</tr>
+	<tr class="g5">
+		<td class="tisi-baqh bold">
+			3
+		</td>
+		<td>
+			Arief Noviarakhman Zagladi (2007)
+			Arti Penting Unsur Keadilan Dalam
+			Organisasi
+			Laporan Penelitian Dipublikasikan
+		</td>
+		<td class="tisi-baqh bold" align="center">10</td>
+		<td> </td>
+		<td> </td>
+		<td>
+			Jurnal Wawasan
+			Kopertis Wilayah XI
+			Volume 13 Nomor 3
+			Desember 2007
+			ISSN : 14411-4267</td>
+	</tr>
+	<tr class="g5">
+		<td class="tisi-baqh bold">
+			4
+		</td>
+		<td>
+			Arief Noviarakhman Zagladi (2007)
+			Arti Penting Unsur Keadilan Dalam
+			Organisasi
+			Laporan Penelitian Dipublikasikan
+		</td>
+		<td class="tisi-baqh bold" align="center">10</td>
+		<td> </td>
+		<td> </td>
+		<td>
+			Jurnal Wawasan
+			Kopertis Wilayah XI
+			Volume 13 Nomor 3
+			Desember 2007
+			ISSN : 14411-4267</td>
+	</tr>
+	<tr class="g5">
+		<td class="tisi-baqh bold">
+			5
+		</td>
+		<td>
+			Arief Noviarakhman Zagladi (2007)
+			Arti Penting Unsur Keadilan Dalam
+			Organisasi
+			Laporan Penelitian Dipublikasikan
+		</td>
+		<td class="tisi-baqh bold" align="center">10</td>
+		<td> </td>
+		<td> </td>
+		<td>
+			Jurnal Wawasan
+			Kopertis Wilayah XI
+			Volume 13 Nomor 3
+			Desember 2007
+			ISSN : 14411-4267</td>
+	</tr>
+</table>
+<?php include('footer3.php');
+?>
