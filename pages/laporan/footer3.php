eb2382d7dcc6f49c0453b45eab60f159e81743a4
gnosprinting <anjar.hexacore@gmail.com>
2017-12-21 17:20:59 +0800

    head3 footer3 for lampiran3

---
+++ pages/laporan/footer3.php
@@ -0,0 +1,60 @@
+<table class="tfa" >
+  <tr>
+    <td class="thaka" width="470"></td>
+    <td width="60">&nbsp;</td>
+    <td width="150" align="left"></td>
+  </tr>
+  <tr>
+    <td rowspan="8">&nbsp;</td>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left">Banjarmasin, 31 Desember 2017</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left">Yang Membuat Pernyataan</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left">Ketua Universitas </td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="" align="center">&nbsp;</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="" align="center">&nbsp;</td>
+  </tr>
+   <tr>
+    <td>&nbsp;</td>
+    <td class="" align="center">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left"><u>Fathul Hafidh</u></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td class="thaka bold" align="left">NIP. 1985110506031</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
---
+++ pages/laporan/head3.php
@@ -0,0 +1,84 @@
+<?php
+$now = date("Y-m-d");
+		// $SQL = "SELECT tunitkerja.* FROM tunitkerja WHERE id_unitkerja=".$_SESSION['4XE']." ";
+		// $myQry = mysqli_query($koneksidb, $SQL)  or die ("Query Lurah : ".mysqli_error($koneksidb));
+		// $datane = mysqli_fetch_array($myQry);
+		// $today = date("Y-m-d");
+?>
+<link href="style-pdf.css" rel="stylesheet" type="text/css" />
+
+<table class="tha" border="0">
+	<tbody>
+		<tr>
+			<td width="500" align="right" rowspan="">LAMPIRAN III</td>
+			<td width="120" colspan="3">KEPUTUSAN BERSAMA MENTERI</td>
+		</tr>
+		<tr>
+			<td width="" align="right" rowspan="5"></td>
+			<td colspan="3">PENDIDIKAN DAN KEBUDAYAAN DAN</td>
+		</tr>
+		<tr>
+			<td colspan="3">KEPALA BADAN KEPEGAWAIAN NEGARA</td>
+		</tr>
+		<tr>
+			<td width="60">NOMOR</td>
+			<td width="5">:</td>
+			<td>61409/MPK/KP/99</td>
+		</tr>
+		<tr>
+			<td>NOMOR</td>
+			<td>:</td>
+			<td>181 TAHUN 1999</td>
+		</tr>
+		<tr>
+			<td>TANGGAL</td>
+			<td>:</td>
+			<td>13 OKTOBER 1999</td>
+		</tr>
+	</tbody>
+</table>
+
+<table class="headtab" align="center" width="760" border="0">
+  <tr>
+    <td width="700" colspan="2" align="center"><h4>DAFTAR KEGIATAN PENELITIAN</h4></td>
+  </tr>
+
+  <tr>
+  <td width="220" align="right"> </td>
+  <td>  </td>
+  </tr>
+
+
+  </table>
+<table class="thap" border="0">
+	<tbody class="thakalh">
+		<tr>
+			<td width="10" rowspan="12"></td>
+			<td width="20" colspan="2">Dosen yang dinilai</td>
+			<td width="5">:</td>
+			<td width="1050"></td>
+		</tr>
+		<tr>
+			<td width="10" rowspan="5"></td>
+			<td width="50">1. Nama</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>2. NIP / NIK </td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>3. Jabatan Fungsional</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>4. Unit Kerja</td>
+			<td>:</td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+<br/>
