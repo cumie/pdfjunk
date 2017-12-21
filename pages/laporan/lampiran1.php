c015965f544ab5fed02473660cbabeec908bf8ab
cumie <havid.aide@gmail.com>
2017-12-22 04:13:14 +0800

    lampiran 1 belum selesai

---
+++ pages/laporan/lampiran1.php
@@ -0,0 +1,141 @@
+<?php 
+session_start(); 
+// include "../../lib/connection.php";
+// include "../../lib/library.php";
+//include_once "../masters/dosen_config.php";
+
+
+// include_once "lib/seslogin.php"; 
+// include_once "pages/masters/dosen_config.php"; 
+// if($_GET) { 
+	  
+// $Kode	 = $_SESSION['ID_CRAB'];
+// $sqlShow = "SELECT * FROM ".$tableName." WHERE ".$field[4]."='$Kode'";
+// $qryShow = mysqli_query($koneksidb, $sqlShow)  or die ("Query ambil data salah : ".mysqli_error($koneksidb));
+// $dataShow = mysqli_fetch_array($qryShow);
+
+// for($i=0;$i<=$jmlField;$i++){
+	// $data[$i] = $dataShow[$field[$i]];
+// }
+// } // Penutup GET
+?>   
+
+<?php include('head1.php');
+?>
+ 
+<table class="tg"> 
+  <tr>
+    <th width="10" class="tg-baqh" rowspan="13">I</th>
+    <th width="250" class="tg-baqh" colspan="7">KETERANGAN PERORANGAN</th>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" width="5">1.</td>
+    <td class="tg-yw4l" width="160"	colspan="2">Nama</td>
+    <td class="tg-yw4l" colspan="4">FATHUL HAFIDH, S.Kom., M.Kom</td>
+  </tr> 
+  <tr>
+    <td class="tg-yw4l">2.</td>
+    <td class="tg-yw4l" colspan="2">NIP/NIDN</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">3.</td>
+    <td class="tg-yw4l" colspan="2">Nomor Seri Karpeg.</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">4.</td>
+    <td class="tg-yw4l" colspan="2">Tempat dan Tanggal Lahir</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">5.</td>
+    <td class="tg-yw4l" colspan="2">Jenis Kelamin</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">6.</td>
+    <td class="tg-yw4l" colspan="2">Pendidikan Tertinggi</td>
+    <td class="tg-yw4l" colspan="4">S2, Universitas Islam Kalimantan Muhammad Arsyad Al Banjari </td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">7.</td>
+    <td class="tg-yw4l" colspan="2">Pangkat dan Golongan Ruang / TMT</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">8.</td>
+    <td class="tg-yw4l" colspan="2">Jabatan Fungsional / TMT</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">9.</td>
+    <td class="tg-yw4l" colspan="2">Fakultas / Jurusan / Program Studi </td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" rowspan="2">10.</td>
+    <td class="tg-yw4l" rowspan="2">Masa Kerja Golongan</td>
+    <td class="tg-yw4l">a.Lama</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">a.Baru</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">11.</td>
+    <td class="tg-yw4l" colspan="2">Unit Kerja</td>
+    <td class="tg-yw4l" colspan="4"></td>
+  </tr>
+  <tr>
+    <td class="tg-baqh" rowspan="5">II</td>
+    <td class="tg-baqh" colspan="7">UNSUR YANG DINILAI</td> 
+  </tr>
+  <tr>
+    <td class="tg-yw4l" rowspan="3">1.</td>
+    <td class="tg-yw4l" colspan="2"> UNSUR UTAMA <br/> A. PENDIDIKAN</td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="2">B. TRIDHARMA PERGURUAN TINGGI 
+	<br/> a. Melaksanakan Pendidikan dan Pengajaran
+	<br/> b. Melaksanakan Penelitian
+	<br/> c. Melaksanakan Pengabdian Kepada Masyarakat
+	</td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+  </tr>
+  <tr>
+    <td class="tg-baqh" colspan="2">JUMLAH</td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">2.</td>
+    <td class="tg-yw4l" colspan="2">UNSUR PENUNJANG <br/> Melaksanakan Penunjang Tugas Pokok Dosen</td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+  </tr>
+  <tr>
+    <td class="tg-baqh" colspan="4">JUMLAH UNSUR UTAMA DAN UNSUR PENUNJANG</td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+    <td class="tg-baqh"></td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">III</td>
+    <td class="tg-yw4l" colspan="5">DAPAT DIANGKAT DALAM JABATAN: </td>
+    <td class="tg-yw4l" colspan="2">TMT.: 01 Mei 2016</td>
+  </tr>
+</table> 
---
+++ pages/laporan/lampiran2-pdf.php
@@ -0,0 +1,16 @@
+<?php
+ob_start();
+ include('lampiran2.php');
+ $content = ob_get_clean();
+ 
+// conversion HTML => PDF
+ require_once('../../plugins/html2pdf/html2pdf.class.php');
+ try
+ {
+ $html2pdf = new HTML2PDF('P','F4','en', false, 'ISO-8859-15',array(15, 10, 10, 10));
+ $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+ $html2pdf->Output('laporan_rekapitulasi.pdf');
+ }
+ catch(HTML2PDF_exception $e) { echo $e; }
+ 
+?>
